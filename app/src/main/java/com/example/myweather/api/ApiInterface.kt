9d7befd68926c61064c6fe3339dc54171e8e33b1
Dinesh Kumar <164221638+Dinesh-singh-saini@users.noreply.github.com>
2024-10-08 22:33:27 +0530

    Update ApiInterface.kt

--- app/src/main/java/com/example/myweather/api/ApiInterface.kt
+++ app/src/main/java/com/example/myweather/api/ApiInterface.kt
@@ -14,6 +14,5 @@ interface ApiInterface {
         @Query("aqi") aqi: String = "yes",
         @Query("alerts") alerts: String = "yes"
     ): Call<WeatherData>
-//  days=3&aqi=yes&alerts=yes
-//  https://api.weatherapi.com/v1/forecast.json?key=47c045fba36a4a9b9d8182320241808&q=18.7988226,84.1329493%20&days=3&aqi=yes&alerts=yes
+
 }
